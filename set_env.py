d65916dcd9aa1ece6d0eb760cd6e7dde249609ce
abuarquemf <abuarquemf@gmail.com>
2020-08-01 12:05:35 -0300

    added app engine deploy resources

---
+++ set_env.py
@@ -0,0 +1,26 @@
+import sys 
+import re
+
+"""This script get DB_HOST, DB_NAME, STORAGE_LOGIN and STORAGE_PASSWORD 
+environment variables used on app engine from Github Secrets and
+replace on app.yaml."""
+
+app_engine_file = "app.yaml"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit("invalid number of arguments: {}".format(len(sys.argv)))
+    db_host = sys.argv[1]
+    db_name = sys.argv[2]
+    storage_login = sys.argv[3]
+    storage_password = sys.argv[4]
+    file_content = ""
+    with open (app_engine_file, "r") as file:
+        app_engine_file_content = file.read()
+        line = re.sub(r"##DB_HOST", db_host, app_engine_file_content) 
+        line = re.sub(r"##DB_NAME", db_name, line)
+        line = re.sub(r"##STORAGE_LOGIN", storage_login, line)
+        line = re.sub(r"##STORAGE_PASSWORD", storage_password, line)
+        file_content = line
+    with open (app_engine_file, "w") as file:
+        file.write(file_content)
